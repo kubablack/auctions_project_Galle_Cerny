d18006509eae10a74c6b4f44aa1d7a153cc8307f
kuba_black <65172859@fsv.cuni.cz>
2021-07-19 10:45:03 +0200

    processor initialised

--- data_processor.py
+++ data_processor.py
@@ -1,1 +1,7 @@
 # tady bude vše co zpracovává data do finální podoby
+
+# udělat to OOP jako je ten dynamic_scraper a importovat do hlavního jupyter notebooku (auctions)
+
+# input co se bude zadávat do metody té třídy musí být list soupů
+
+# export té třídy musí být ideálně csv nebo tak 
