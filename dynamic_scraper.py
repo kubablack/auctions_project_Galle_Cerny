29bead41931fed1e0f24e8f76c2731dd92247343
kuba_black <65172859@fsv.cuni.cz>
2021-08-23 20:59:40 +0200

    final version

--- dynamic_scraper.py
+++ dynamic_scraper.py
@@ -12,19 +12,63 @@ class DataDownloader:
     '''
     This class crawls through dynamic content of https://www.portaldrazeb.cz and collects following things:
 
-            1) link to every auction + auction category (since the category is not within the auction page itself)
-            2) list of all possible values from drop-down menu (auction categories, regions and districts)
+            1) soup object for every auctioneer
+            2) link to every auction + auction category (since the category is not within the auction page itself)
+            3) list of all possible values from drop-down menu (auction categories, regions and districts)
+    
+    Subsequently it extracts soup objects of all auctions from the links collected in 2).
     '''
 
     def __init__(self):
         # we initiate the lists for data within particular methods
+        self.auctioneers_soups = []
         self.auction_links_and_categories = []
         self.categories = []
         self.regions_and_districts = []
         print('Downloader successfully initialized!')
         print(' ')
         print(DataDownloader.__doc__)
+    
+    def get_soups_of_auctioneers(self,link):
+        '''
+        Crawls through all pages of auctioneers and creates a soup object of everyone that is listed there right now.
+        '''
+        # initiating a webdriver
+        driver = webdriver.Chrome('./chromedriver') 
+        
+        # opening the link in Chrome
+        driver.get(link)  
+        time.sleep(5) 
+        
+        # creating a soup object from the page source code
+        soup = BeautifulSoup(driver.page_source, "html.parser")  
+        
+        # getting number of pages
+        last_page = int(soup.find('div',{'class':'el-pagination'}).findAll('li',{'class':'number'})[-1].text) 
+        
+        # locating the element into which we write page number
+        page_number = driver.find_element_by_css_selector('input[type="number"]')
+        
+        # looping through all pages and save the soups
+        for page in tqdm(range(1,last_page+1)):
+            # getting to a page (delete content, send number of the page, press Enter)
+            page_number.send_keys(Keys.BACK_SPACE)
+            page_number.send_keys(Keys.BACK_SPACE)
+            page_number.send_keys(str(page)) 
+            page_number.send_keys(Keys.RETURN)
+            time.sleep(5)  
 
+            # save soups of particular auctioneers
+            html = driver.page_source   
+            soup = BeautifulSoup(html, features="lxml") # soup of current page  
+            for i in soup.findAll('article'):
+                self.auctioneers_soups.append(i) # extract all auctioneers
+                
+        # close the window and check soups
+        driver.close()
+        if len(self.auctioneers_soups)>50:
+                print(f'Soup objects of auctioneers successfully downloaded! There are {len(self.auctioneers_soups)} of them right now.')
+                
     def get_auction_links_and_categories(self, link):
         '''
         Method that rawls through all pages of auctions and from their source codes then collects link and category of every auction.
@@ -63,9 +107,12 @@ class DataDownloader:
                 auction = []
                 auction.append(i.find('a')['href'])
 
-                # extracting categories
-                categ = soup.find('article').find('tbody').findAll('tr')[1].find('span').text.lstrip('/').split('/')
+                # extracting categories and region
+                categ = i.find('tbody').findAll('tr')[1].find('span').text.lstrip('/').split('/')
                 auction.append(categ)
+                region = i.find("th",string="Okres:").find_next_sibling().text
+                auction.append(region)
+                
                 self.auction_links_and_categories.append(auction)  # saving the data
 
         # closing the window and checking whether something downloaded
